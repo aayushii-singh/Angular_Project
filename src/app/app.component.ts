b1674320d35952da2cfd26cf33098bebcf0fd2a6
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-03-22 16:35:07 +0530

    Added: used binded technique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
+  bridgeLabzLogo = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20Brand%20Logo.svg'; // Logo URL
+  bridgeLabzURL = 'https://www.bridgelabz.com'; // Website URL
+
+  openBridgeLabzSite() {
+    window.open(this.bridgeLabzURL, '_blank'); // Opens in a new tab
+  }
 }
